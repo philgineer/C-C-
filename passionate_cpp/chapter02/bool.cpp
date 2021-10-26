68ff36781a7f15fe054e04593fbb082f5b7044f2
Junho Yun <43261253+philgineer@users.noreply.github.com>
2021-10-26 22:38:10 +0900

    studied

---
+++ passionate_cpp/chapter02/bool.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool IsPositive(int num) {
+	if (num <= 0)
+		return false;
+	else
+		return true;
+}
+
+int main(void) {
+	int	num = 10;
+	int	i = 0;
+
+	cout<<"true: "<<true<<endl;
+	cout<<"false: "<<false<<endl;
+
+	while(true) {
+		cout<<i++<<' ';
+		if (i > num)
+			break;
+	}
+	cout<<endl;
+
+	cout<<"sizeof(1): "<<sizeof(1)<<endl;
+	cout<<"sizeof(0): "<<sizeof(0)<<endl;
+	cout<<"sizeof(true): "<<sizeof(true)<<endl;
+	cout<<"sizeof(false): "<<sizeof(false)<<endl;
+	
+	cout<<"IsPositive(3): "<<IsPositive(3)<<endl;
+
+	return 0;
+}
